2887b1596538cf8951b52927d8e5eded5a5e5ddc
spuersant271 <santiagoabad.clementearredondo.gen23@cbtis105.edu.mx>
2025-04-02 10:03:18 -0600

    casi

--- Controllers/Api/EqController.cs
+++ Controllers/Api/EqController.cs
@@ -1,9 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
 [ApiController]
-Route("api/eq")
+[Route("api/eq")]
 public class EqController : Controller{
-    [HTTpGet("listar-agencia ")]
+    [HttpGet("listar-agencia")]
     public IActionResult ListarAgencia (){
-        return OK();
+        //Listar todos los registros de la agencia Torres Really
+
+        MongoClient client = new MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase ("Inmuebles");
+        var collection = db.GetCollection<Inmueble>("RentasVentas");
+
+        var lista = collection.Find(FilterDefinition<Inmueble>.Empty).ToList();
+
+        return Ok(lista);
 
     }
     
